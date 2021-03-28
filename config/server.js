219f7aded1f1dd2d26e1e97476f9da2c0df5254c
Roman <roman.yakovlev@codingteam.ru>
2021-03-28 12:14:39 +0300

    Added url for heroku

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_HOST', "https://mestob.herokuapp.com"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '454e7851472af487753fcde9af5ae8de'),
